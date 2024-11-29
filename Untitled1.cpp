23ef99d8d2955212cd6656c18946927f6207f049
minhha-0102 <minhhabuonacctop1sv@gmail.com>
2024-11-29 22:35:24 +0700

    project 1

---
+++ Untitled1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+	int a;
+	printf("nhap vao mot so nguyen:");
+	scanf("%d", &a);
+	if (a % 2 == 0){
+		printf("so %d la so chan.\n", a);
+	} else {
+		printf("so %d la so le.\n", a);
+	}
+//
+	
+}
